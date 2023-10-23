5a7c32252dc5b590370058b7068e89081ba81ed4
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-10-23 22:07:13 +0300

    Update SQL promts

--- INFO/SQLs.sql
+++ INFO/SQLs.sql
@@ -24,12 +24,19 @@ CREATE TABLE IF NOT EXISTS orders
 INSERT INTO
  fruits (img, name, article, description, price)
 VALUES
- ('apple.jpg', 'Apples', 'frt-0001', 'Apples Ukraine Honey Crisp', 1.50),
- ('banana.jpg', 'Bananas', 'frt-0002', 'Bananas Brazil', 0.75),
- ('grapefruit.jpg', 'Grapefruits', 'frt-0003', 'Grapefruits Caribbeans', 2.50),
- ('mango.jpg', 'Mangoes', 'frt-0004', 'Mangoes Egypt', 3.00),
- ('oranges.jpg', 'Oranges', 'frt-0005', 'Oranges Spain', 2.20),
- ('pineapples.jpg', 'Pineapples', 'frt-0006', 'Pineapples Costa-Rica', 4.50),
- ('pomegranate.jpg', 'Pomegranate', 'frt-0007', 'Pomegranate Georgia', 2.10),
- ('white_grape.jpg', 'White Grape', 'frt-0008', 'White Grape Ukraine', 1.20);
+ ('fruit-apple.jpg', 'Apples', 'frt-0001', 'Apples Ukraine Honey Crisp', 1.50),
+ ('fruit-banana.jpg', 'Bananas', 'frt-0002', 'Bananas Brazil', 0.75),
+ ('fruit-grapefruit.jpg', 'Grapefruits', 'frt-0003', 'Grapefruits Caribbeans', 2.50),
+ ('fruit-mango.jpg', 'Mangoes', 'frt-0004', 'Mangoes Egypt', 3.00),
+ ('fruit-oranges.jpg', 'Oranges', 'frt-0005', 'Oranges Spain', 2.20),
+ ('fruit-pineapples.jpg', 'Pineapples', 'frt-0006', 'Pineapples Costa-Rica', 4.50),
+ ('fruit-pomegranate.jpg', 'Pomegranate', 'frt-0007', 'Pomegranate Georgia', 2.10),
+ ('fruit-white_grape.jpg', 'White Grape', 'frt-0008', 'White Grape Ukraine', 1.20);
 
+SELECT * FROM products;
+
+DELETE FROM products WHERE id = :id;
+
+SELECT * FROM orders;
+
+DELETE FROM orders WHERE id = :id;
