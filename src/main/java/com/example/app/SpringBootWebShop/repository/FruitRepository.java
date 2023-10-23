1bb7081eaa8ca4cd7bd3f1de9d9efef8e051007c
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-10-23 22:09:20 +0300

    Rename repository interface

--- src/main/java/com/example/app/SpringBootWebShop/repository/FruitRepository.java
+++ src/main/java/com/example/app/SpringBootWebShop/repository/FruitRepository.java
@@ -4,8 +4,10 @@ import com.example.app.SpringBootWebShop.entity.Fruit;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface FruitRepository extends CrudRepository<Fruit, Long> {
-
+    List<Fruit> findAll();
 }
