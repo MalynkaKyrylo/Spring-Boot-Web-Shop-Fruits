22aef05f4c403558e9449cd74d0851d009d4a91e
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-10-23 22:09:54 +0300

    Refactor AdminFruitService class

--- src/main/java/com/example/app/SpringBootWebShop/service/Admin/AdminFruitService.java
+++ src/main/java/com/example/app/SpringBootWebShop/service/Admin/AdminFruitService.java
@@ -12,9 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -32,7 +32,7 @@ public class AdminFruitService {
     @Autowired
     FruitRepository repository;
 
-    public ResponseEntity<?> addFruit(String[] data, MultipartFile file) throws IOException {
+    public ResponseEntity<?> addFruit(String[] data, MultipartFile multipartFile) throws IOException {
         Map<String, String> errors = validateData(data);
         if (!errors.isEmpty()) {
             try {
@@ -48,17 +48,13 @@ public class AdminFruitService {
             fruit.setArticle(data[1]);
             fruit.setDescr(data[2]);
             fruit.setPrice(Double.parseDouble(data[3]));
+
             String fileUpload = imgPrefix + "-" + data[4];
+            byte[] bytes = multipartFile.getBytes();
+            Path path = Paths.get(Constants.URL_TO_UPLOADS + fileUpload);
+            Files.write(path, bytes);
 
-            String directory = Constants.URL_FILE_UPLOADS;
-            String filepath = Paths.get(directory, fileUpload).toString();
-
-            LOGGER.info("Admin filepath: " + filepath);
-
-            BufferedOutputStream stream =
-                    new BufferedOutputStream(new FileOutputStream(filepath));
-            stream.write(file.getBytes());
-            stream.close();
+            LOGGER.info("Upload path info: " + path);
 
             fruit.setImg(fileUpload);
             repository.save(fruit);
@@ -68,17 +64,25 @@ public class AdminFruitService {
         }
     }
 
+    // TODO develop
+    public ResponseEntity<?> updateFruit(){
+        return null;
+    }
+
+    // TODO develop
+    public ResponseEntity<?> deleteFruit(){
+        return null;
+    }
+
+    // TODO requires some extra work to validate the file
+
     public List<Fruit> getAll() {
         Iterable<Fruit> iterable = repository.findAll();
         List<Fruit> list =
                 StreamSupport.stream(iterable.spliterator(), false)
-                        .map(fruit -> new Fruit(fruit.getId(),
-                                Constants.BASE_URL +
-                                        Constants.URL_IMAGES_UPLOADS + fruit.getImg(),
-                                fruit.getName(),
-                                fruit.getArticle(),
-                                fruit.getDescr(),
-                                fruit.getPrice()))
+                        .map(fruit -> new Fruit(fruit.getId(), fruit.getImg(),
+                                fruit.getName(), fruit.getArticle(),
+                                fruit.getDescr(), fruit.getPrice()))
                         .toList();
         return new ArrayList<>(list);
     }
